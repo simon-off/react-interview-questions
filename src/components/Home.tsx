9dcf55c9f84a7113322b8ecf66f5fd33ce89b29e
simonoff <hellosimonoff@gmail.com>
2023-10-10 21:13:50 +0200

    added mobile menu and made site more responsive

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,3 +1,17 @@
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <article>
+      <h1>Home</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis,
+        deleniti?
+      </p>
+      <hr />
+      <p>
+        Voluptatibus quis consequatur hic reprehenderit, officia adipisci ipsam
+        error neque deserunt ullam amet illo. Rem earum, explicabo natus
+        recusandae incidunt fugiat minima quas distinctio voluptas?
+      </p>
+    </article>
+  );
 }
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,38 +1,68 @@
 import { Link, Outlet } from "react-router-dom";
-import { Binary, Menu } from "lucide-react";
+import { Binary, Menu, X } from "lucide-react";
+import { useEffect, useRef, useState } from "react";
+import useWindowResize from "../hooks/useWindowResize";
 
 export default function Layout() {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [headerHeight, setHeaderHeight] = useState(0);
+  const headerRef = useRef<HTMLElement>(null);
+
+  useWindowResize(() => {
+    if (window.innerWidth > 1000) {
+      setMenuOpen(false);
+    } else {
+      setHeaderHeight(headerRef?.current?.offsetHeight ?? 0);
+    }
+  }, true);
+
+  // Used to set (position: "fixed") on body when burger menu is open
+  useEffect(() => {
+    if (menuOpen) {
+      document.body.classList.add("open");
+    } else {
+      document.body.classList.remove("open");
+    }
+  }, [menuOpen]);
+
   return (
     <>
-      <header>
+      <header ref={headerRef}>
         <Link to="/">
           <Binary />
           React Interview Questions
         </Link>
         <div>
-          <button>
+          <button onClick={() => setMenuOpen(!menuOpen)}>
             <Menu />
           </button>
-          <p>
+          <span>
             coded by{" "}
             <a target="_blank" href="https://simonoff.se">
               simonoff
             </a>
-          </p>
+          </span>
         </div>
       </header>
       <main>
-        <aside>
-          <h2>Pages</h2>
+        <aside className={menuOpen ? "open" : ""} style={{ top: headerHeight }}>
+          <h2>
+            Questions
+            <button onClick={() => setMenuOpen(false)}>
+              <X />
+            </button>
+          </h2>
           <nav>
-            <Link to="/">Home</Link>
-            <Link to="/">Home</Link>
+            <Link to="/">Transfer List</Link>
+            <Link to="/blogs">Country Game</Link>
             <Link to="/blogs">blogs</Link>
-            <Link to="/blogs">blogs</Link>
+            <Link to="/blogs">LAST</Link>
           </nav>
         </aside>
-
-        <div className="container">
+        <div
+          onClick={() => setMenuOpen(false)}
+          className={`container page-content ${menuOpen ? "open" : ""}`}
+        >
           <Outlet />
         </div>
       </main>
