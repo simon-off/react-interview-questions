b546d0afa7d176a23de472c5e934c8ef1063a27e
simonoff <hellosimonoff@gmail.com>
2023-10-15 12:18:18 +0200

    updated home page markup

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,32 +3,27 @@ export default function Home() {
     <article>
       <h1>What is this site?</h1>
       <p>
-        This site contains a collection of <b>React Interview Questions</b> I've
-        collected from various sources on the internet. I made it so that I
-        could practice and learn more about React and JavaScript in general.
+        This site contains a collection of <b>React Interview Questions</b> I've collected from
+        various sources on the internet. I made it so that I could practice and learn more about
+        React and JavaScript in general.
       </p>
       <p>
-        If you're at a similar stage in your frontend developer journey, perhaps
-        it can be useful to you as well!
+        If you're at a similar stage in your frontend developer journey, perhaps it can be useful to
+        you as well!
       </p>
       <h2>Where's the good stuff?</h2>
       <p>
-        In the navigation to the left you will find all the challenges. Each one
-        has its own page with info of where I found it, instructions on how to
-        implement it and the result of my own implementation.
+        In the navigation to the left you will find all the challenges. Each one has its own page
+        with info of where I found it, instructions on how to implement it and the result of my own
+        implementation.
       </p>
       <p>
-        If you want to dig into the code and see how I solved the challenges you
-        can check out{" "}
-        <a
-          target="_blank"
-          href="https://github.com/simon-off/react-interview-questions"
-        >
+        If you want to dig into the code and see how I solved the challenges you can check out{" "}
+        <a target="_blank" href="https://github.com/simon-off/react-interview-questions">
           the repo over on GitHub
         </a>
-        . It contains all the source code for the challenges as well as this
-        entire site. Thank you for checking it out and good luck on your own
-        React journey!
+        . It contains all the source code for the challenges as well as this entire site. Thank you
+        for stopping by and good luck on your own React journey!
       </p>
     </article>
   );
